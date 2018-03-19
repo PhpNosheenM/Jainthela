806583142aef827deea784e9cb9a63b8c30d9fb0
Shelunagori <shelunagori@gmail.com>
2018-03-19 11:34:51 +0530

    Item List Api

--- src/Controller/Api/CategoriesController.php
+++ src/Controller/Api/CategoriesController.php
@@ -28,17 +28,23 @@ class CategoriesController extends AppController
 
 		if(!empty($category_id) and (!empty($city_id)) and (!empty($page))){
 
-			$data = $this->Categories->find()->where(['id'=>$category_id,'city_id'=>$city_id,'section_show'=>'Yes','status'=>'Active'])->contain(['ChildCategories']);
-
-				if($data->toArray()){
-
-					$this->set(['success' => true,'message'=>'Data Found Successfully','data' => $data,'_serialize' => ['success','message','data']]);
-
-				}else{
-					$data=[];
-					$this->set(['success' => false,'message'=>'Data Not Found','data' => $data,'_serialize' => ['success','message','data']]);
-				}
-
+			// CheckAvabiltyOfCity function is avaliable in app controller for checking city_id in cities table
+			$isValidCity = $this->CheckAvabiltyOfCity($city_id);
+			if($isValidCity == 0)
+			{
+				$data = $this->Categories->find()->where(['id'=>$category_id,'city_id'=>$city_id,'section_show'=>'Yes','status'=>'Active'])->contain(['ChildCategories']);
+					if($data->toArray()){
+							$this->set(['success' => true,'message'=>'Data Found Successfully','data' => $data,'_serialize' => ['success','message','data']]);
+					}else{
+						$data=[];
+						$this->set(['success' => false,'message'=>'Data Not Found','data' => $data,'_serialize' => ['success','message','data']]);
+					}
+			}else {
+							$data=[];
+							$success = false;
+							$message = 'Invalid City';
+							$this->set(['success' => $success,'message'=>$message,'data' => $data,'_serialize' => ['success','message','data']]);
+			}
 		}else{
 			$data=[];
 			$this->set(['success' => false,'message'=>'Empty Category_id or city_id or page','data' => $data,'_serialize' => ['success','message','data']]);
--- src/Controller/Api/ItemsController.php
+++ src/Controller/Api/ItemsController.php
@@ -18,9 +18,42 @@ class ItemsController extends AppController
     public function initialize()
      {
          parent::initialize();
-         $this->Auth->allow(['productDetail']);
+         $this->Auth->allow(['productDetail','itemList']);
      }
 
+     public function itemList($category_id=null,$city_id=null)
+     {
+       $city_id = @$this->request->query['city_id'];
+       $category_id = @$this->request->query['category_id'];
+
+       if(!empty($city_id) && !empty($category_id))
+       {
+         // CheckAvabiltyOfCity function is avaliable in app controller for checking city_id in cities table
+         $isValidCity = $this->CheckAvabiltyOfCity($city_id);
+         if($isValidCity == 0)
+         {
+           $items = $this->Items->find()
+                     ->contain(['Categories','ItemVariations'=>['Units']])
+                     ->where(['Items.status'=>'Active','Items.approve'=>'Approved','Items.ready_to_sale'=>'Yes','Items.section_show'=>'Yes','Items.city_id'=>$city_id,'Items.category_id'=>$category_id]);
+               if(!empty($items->toArray()))
+               {
+                 $success = true;
+                 $message = 'Data Found Successfully';
+               }
+               else {
+                     $success = false;
+                     $message = 'Record not found';
+               }
+         }else {
+                 $success = false;
+                 $message = 'Invalid City';
+         }
+       }else {
+         $success = false;
+         $message = 'Record not found';
+       }
+       $this->set(['success' => $success,'message'=>$message,'items' => $items,'_serialize' => ['success','message','items']]);       
+     }
 
     public function productDetail($item_id = null,$city_id =null,$category_id=null)
     {
