0b82c75d40ba56519c35f927c58dd8ac1df06843
Gopesh Parihar <gopeshparihar7@gmail.com>
2018-07-02 17:53:11 +0530

    gpsggyy changes  sh sedreffv xvdsv dsv fvc

--- src/Controller/CustomersController.php
+++ src/Controller/CustomersController.php
@@ -60,7 +60,12 @@ class CustomersController extends AppController
     {
 		$user_id=$this->Auth->User('id');
 		$city_id=$this->Auth->User('city_id');
-		$this->viewBuilder()->layout('admin_portal');
+		$user_type=$this->Auth->User('user_type');
+		if($user_type=='Super Admin'){
+			$this->viewBuilder()->layout('super_admin_layout');
+		}else if($user_type=='Admin'){
+			$this->viewBuilder()->layout('admin_portal');
+		}
         $this->paginate = [
             'contain' => ['Cities'],
 			'limit' => 20
